22028f0199497f4c29b4e2b0a576baddc9ee6250
sujay-hub <ssujay93@gmail.com>
2026-03-09 22:32:52 +0530

    await written inside async function for bcrypt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,15 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const salt = await bcrypt.genSalt(10);
-const hashedPassword = await bcrypt.hash(password, salt);
-
 
 /* Signup */
 exports.signup = async (req, res) => {
     try {
-
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
         const { name, email, password } = req.body;
-
         const existingUser = await User.findOne({ email });
+
 
         if (existingUser) {
             return res.status(400).json({ message: "User already exists" });
