dedfdc72da912a5b459e1090993e85bde72efc54
sujay-hub <ssujay93@gmail.com>
2026-03-09 22:37:21 +0530

    authcontroller hashedpassword written twice removed.

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,8 +15,6 @@ exports.signup = async (req, res) => {
             return res.status(400).json({ message: "User already exists" });
         }
 
-        const hashedPassword = await bcrypt.hash(password, 10);
-
         const user = new User({
             name,
             email,
